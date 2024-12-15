ca5f58be5a979ae110530c08b45a07f70f72e4e1
Kirill Magonov <magonov09@bk.ru>
2024-12-15 19:48:24 +0300

    feat: added render in init app

--- src/utilities/init.js
+++ src/utilities/init.js
@@ -1,5 +1,7 @@
 import createbuttonAddNote from "../button-add-note/create-button-add-note.js";
 import createHeader from "../header/header-view.js";
+import { data } from "./data-handler.js";
+import { clearRender, render } from "./render.js";
 
 const initApp = () => {
     const containerApp = document.body;
@@ -9,6 +11,10 @@ const initApp = () => {
 
     const buttonAddNote = createbuttonAddNote();
     containerApp.insertAdjacentElement("beforeend", buttonAddNote);
+
+    clearRender();
+    render(data.favoritesNotes);
+    render(data.regularNotes);
 };
 
 export default initApp;
